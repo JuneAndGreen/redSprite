c9d7bb0e7383e8cb5075321e0d4879b3313604d8
hzxiejin <hzxiejin@corp.netease.com>
2016-01-14 20:20:45 +0800

    add test cast

--- test/test.js
+++ test/test.js
@@ -1,21 +1,34 @@
 var sprite = require('./../index');
 var path = require('path');
+var path = require('path');
+var should = require('should');
 
-var obj = {
-	filemap: {},
-	margin: 0,
-	orientation: 0,
-	template: null,
-	infofile: null,
-	sort: true
-};
-obj.filemap[path.join(__dirname, './output/out.png')] = [
-	path.join(__dirname, './input/0.png'),
-	path.join(__dirname, './input/1.png'),
-	path.join(__dirname, './input/2.png'),
-	path.join(__dirname, './input/3.png')
-];
+describe('sprite the images',function(){
+  it('should return the result iamge info',function(){
+		var obj = {
+			filemap: {},
+			margin: 0,
+			orientation: 0,
+			template: null,
+			infofile: null,
+			sort: true
+		};
+		obj.filemap[path.join(__dirname, './output/out.png')] = [
+			path.join(__dirname, './input/0.png'),
+			path.join(__dirname, './input/1.png'),
+			path.join(__dirname, './input/2.png'),
+			path.join(__dirname, './input/3.png')
+		];
 
-sprite.create(obj, function(res) {
-	console.log(JSON.stringify(res, null, '\t'));
+		var shouldRes = {};
+		var map = shouldRes[path.join(__dirname, './output/out.png')] = {};
+		map[path.join(__dirname, './input/0.png')] = {width:64,height:64,x:0,y:0};
+		map[path.join(__dirname, './input/1.png')] = {width:64,height:64,x:0,y:64};
+		map[path.join(__dirname, './input/2.png')] = {width:64,height:64,x:0,y:128};
+		map[path.join(__dirname, './input/3.png')] = {width:64,height:64,x:0,y:192};
+
+		sprite.create(obj, function(res) {
+			res.should.equal(map);
+		});
+	});
 });
