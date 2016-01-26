086cad7d812d42f7791861f3591296d75eba8e16
hzxiejin <hzxiejin@corp.netease.com>
2016-01-26 13:53:42 +0800

    modify template parser

--- src/parse.js
+++ src/parse.js
@@ -3,6 +3,38 @@ var parse = {
    * 解析模板为执行函数
    */
   get: (function() {
+    var regmap = [
+      // if语句开始
+      {reg: /^if\s+(.+)/i, val: function(all, condition) {return 'if(' + condition + ') {';}},
+      // elseif 语句开始
+      {reg: /^elseif\s+(.+)/i, val: function(all, condition) {return '} else if(' + condition + ') {';}},
+      // else语句结束
+      {reg: /^else/i, val: '} else {'},
+      // if语句结束
+      {reg: /^\/\s*if/i, val: '}'},
+      // list语句开始
+      {reg: /^list\s+([\S]+)\s+as\s+([\S]+)/i, val: function(all, arr, item) {return 'for(var __INDEX__=0;__INDEX__<' + arr + '.length;__INDEX__++) {var ' + item + '=' + arr + '[__INDEX__];var ' + item + '_index=__INDEX__;';}},
+      // list语句结束
+      {reg: /^\/\s*list/i, val: '}'},
+      // var 语句
+      {reg: /^var\s+(.+)/i, val: function(all, expr) {return 'var ' + expr + ';';}}
+    ];
+    /*
+     * 转换模板语句
+     */
+    var transStm = function(stmJs) {
+      stmJs = stmJs.trim();
+      for(var i=0; i<regmap.length; i++) {
+        var item = regmap[i];
+        if(item.reg.test(stmJs)) {
+          return (typeof item.val === 'function') ? stmJs.replace(item.reg, item.val) : item.val;
+        }
+      }
+    };
+
+    /*
+     * 解析模板
+     */
     var doParseTemplate = function(content, data) {
       content = content.replace(/\t/g, '  ').replace(/\n/g, '\\n').replace(/\r/g, '\\r');
       // 初始化模板生成器结构
@@ -27,33 +59,49 @@ var parse = {
       var stmbeg = 0;  // 表达式起始位置
       var stmend = 0; // 表达式结束位置
       var len = content.length;
-      var preHtml = ''; // 表达式前的html
-      var endHtml = ''; // 最后一段html
+      var preCode = ''; // 表达式前的代码
+      var endCode = ''; // 最后一段代码
       var stmJs = ''; // 表达式
       while(beg < len) {
-        stmbeg = content.indexOf('<%', beg);
+        /* 开始符 */
+        stmbeg = content.indexOf('{', beg);
+        while(content.charAt(stmbeg - 1) === '\\') {
+          // 遇到转义的情况
+          stmbeg = content.indexOf('{', stmbeg + 1);
+        }
         if(stmbeg === -1) {
-          // 到达最后一段html
-          endHtml = content.substr(beg);
-          out.push('OUT.push(\'' + endHtml + '\');');
+          // 到达最后一段代码
+          endCode = content.substr(beg);
+          out.push('OUT.push(\'' + endCode + '\');');
           break;
         }
-        stmend = content.indexOf('%>', stmbeg);
+
+        /* 结束符 */
+        stmend = content.indexOf('}', stmbeg);
+        while(content.charAt(stmend - 1) === '\\') {
+          // 遇到转义的情况
+          stmend = content.indexOf('}', stmend + 1);
+        }
         if(stmend === -1) {
+          // 没有结束符
           break;
         }
-        preHtml = content.substring(beg, stmbeg);
-        out.push('OUT.push(\'' + preHtml + '\');');
-        if(content.charAt(stmbeg + 2) === '=') {
+
+        // 开始符之前代码 
+        preCode = content.substring(beg, stmbeg);
+
+        if(content.charAt(stmbeg - 1) === '$') {
           // 针对变量取值
-          stmJs = content.substring(stmbeg + 3, stmend);
+          out.push('OUT.push(\'' + preCode.substr(0, preCode.length-1) + '\');');
+          stmJs = content.substring(stmbeg + 1, stmend);
           out.push('OUT.push((' + stmJs + ').toString());');
         } else {
           // 针对js语句
-          stmJs = content.substring(stmbeg + 2, stmend);
-          out.push(stmJs);
+          out.push('OUT.push(\'' + preCode + '\');');
+          stmJs = content.substring(stmbeg + 1, stmend);
+          out.push(transStm(stmJs));
         }
-        beg = stmend + 2;
+        beg = stmend + 1;
       }
       out.unshift(vars);
       struct[1] = out.join('');
